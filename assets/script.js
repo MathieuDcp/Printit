981f65d6097dc7e2433b2860d2460c4fb616999c
MathieuDcp <descarpentries.mathieu85@gmail.com>
2023-06-02 11:13:36 +0200

    Version 2

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,46 @@
-
 let slideIndex = 1;
-	showSlides(slideIndex);
+// Fonction pour changer de slide avec fléches et dots
+function ChangerSlides(nbr) {
+  const slides = document.getElementsByClassName("slides");
+  slideIndex += nbr;
+  AfficherSlides(slideIndex);
+}
 
-	function plusSlides(n) {
-		showSlides(slideIndex += n);
-		showSlides.style.transition = "all 0.5s ease";
-	}
+// Fonction pour la slide actuelle
+function currentSlide(nbr) {
+  slideIndex = nbr;
+  AfficherSlides(slideIndex);
+}
 
-	function currentSlide(n) {
-		showSlides(slideIndex = n);
-	}
-	function showSlides(n) {
-		let slides = document.getElementsByClassName('slides');
-		let dots = document.getElementsByClassName('dot');
+// Fonction permettant d'afficher les slides avec fléches et doits
+function AfficherSlides(nbr) {
+  const slides = document.getElementsByClassName("slides");
+  const dots = document.getElementsByClassName("dot");
 
-		if (n > slides.length) { slideIndex = 1; }
+// Vérifie si nbr est supérieur à la slide actuelle
+  if (nbr > slides.length) {
+    slideIndex = 1;
+  }
 
-		if (n < 1) { slideIndex = slides.length; }
+  // Vérifie si nbr est inférieur à la slide actuelle
+  if (nbr < 1) {
+    slideIndex = slides.length;
+  }
 
-		// Cacher toutes les slides
-		for (let i = 0; i < slides.length; i++) {
-			slides[i].style.display = "none";
-		}
+  // Cacher toutes les slides
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-		// Retirer "active" de tous les points
-		for (let i = 0; i < dots.length; i++) {
-			dots[i].classList.remove('active');
-		}
+  // Retirer "active" de tous les points
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+  // Afficher la slide demandée
+  slides[slideIndex - 1].style.display = "block";
 
-		// Afficher la slide demandée
-		slides[slideIndex - 1].style.display='block';
+  // Ajouter "active" sur le point cliqué
+  dots[slideIndex - 1].classList.add("active");
+}
 
-		// Ajouter "active" sur le point cliqué
-		dots[slideIndex - 1].classList.add('active');
-	}
+AfficherSlides(slideIndex);
